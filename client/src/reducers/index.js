c4058b2ef3d871f9093f9cd878dfe68224a9a7f5
Natan Z <natan.zaizev@gmail.com>
2020-09-09 00:41:58 +0300

    integrated RESTful api to save user created streams, some ui tweaks as well

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
-import { reducer } from "redux-form";
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
 	auth: authReducer,
-	form: reducer,
+	form: formReducer,
+	streams: streamReducer,
 });
