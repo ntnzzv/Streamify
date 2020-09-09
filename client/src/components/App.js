46142b7629e25e2ca0d5b9acfd195dda2491a35f
Natan Z <natan.zaizev@gmail.com>
2020-09-09 21:12:44 +0300

    added db

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
@@ -11,14 +11,14 @@ import history from "../history";
 const App = () => {
 	return (
 		<div className='ui container'>
-			<BrowserRouter history={history}>
+			<Router history={history}>
 				<Header />
 				<Route path='/' exact component={StreamList} />
 				<Route path='/streams/new' exact component={StreamCreate} />
 				<Route path='/streams/edit' exact component={StreamEdit} />
 				<Route path='/streams/delete' exact component={StreamDelete} />
 				<Route path='/streams/show' exact component={StreamShow} />
-			</BrowserRouter>
+			</Router>
 		</div>
 	);
 };
